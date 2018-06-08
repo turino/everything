5808986930e1dce614b96d099f1851d196b74719
Jacob Turino <jacob@thoughtbot.com>
2018-06-08 13:20:59 -0700

    Configure RSpec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,15 @@
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
+abort("DATABASE_URL environment variable is set") if ENV["DATABASE_URL"]
+
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
